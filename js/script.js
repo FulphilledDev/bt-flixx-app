fcb124f7994e5c7ee7607cf06b954e253addb93f
Philip Simpson <dev.phits@gmail.com>
2023-07-08 16:28:19 -0700

    Fetch API & Data; get popular movies

--- js/script.js
+++ js/script.js
@@ -4,6 +4,27 @@ const state = {
   currentPage: window.location.pathname,
 };
 
+async function displayPopularMovies() {
+  // Destructuring results returns the 'results' array from the data object returned
+  const { results } = await fetchAPIData('movie/popular');
+  console.log(results);
+}
+
+// Fetch data from TMDB API
+// NOTE: FOR PRODUCTION--> create own backend server and make the request to THAT server where API Key is stored, then request from there to TMDB DB
+async function fetchAPIData(endpoint) {
+  const API_KEY = '9b2908a826b16e2c7df1ea12209ccc7d';
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  const data = await response.json();
+
+  return data;
+}
+
 // Highlight Active link
 function highlightActiveLink() {
   const links = document.querySelectorAll('.nav-link');
@@ -21,7 +42,8 @@ function init() {
   switch (state.currentPage) {
     case '/':
     case '/index.html':
-      console.log('Home');
+      //   console.log('Home');
+      displayPopularMovies();
       break;
     case '/shows.html':
       console.log('Shows');
